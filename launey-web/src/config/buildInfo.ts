a84ba86b70bd493c350f11012a254815e47aab2d
4roff <4roffdesign@gmail.com>
2026-06-22 21:59:50 +0300

    Remove legacy runtime files from repository

--- launey-web/src/config/buildInfo.ts
+++ launey-web/src/config/buildInfo.ts
@@ -3,5 +3,5 @@ export const APP_VERSION = '1.0.0'
 export const BUILD_INFO = {
   appName: 'Launey',
   appVersion: APP_VERSION,
-  lastUpdatedAt: '2026-06-15T03:21:10.036Z',
+  lastUpdatedAt: '2026-06-22T02:23:08.758Z',
 } as const
